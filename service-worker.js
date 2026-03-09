404e209ef8619f601fc6612442a8bb3d3a0aa9b6
jeelnotme <samanttanicole13@gmail.com>
2026-03-09 13:23:24 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open("todo-list-cache").then(function(cache) {
+    caches.open("budget-cache").then(function(cache) {
       return cache.addAll(["index.html"]);
     })
   );
